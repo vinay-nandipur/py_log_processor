0e279e7d2c8696fbe27c981fe797099fd31c6e08
vinay-nandipur <vinay.nandipur@gmail.com>
2020-08-09 20:19:03 +0530

    get filename without extension

--- log_processor.py
+++ log_processor.py
@@ -2,7 +2,7 @@
 import os
 import requests
 
-
+# Download log archive
 url = 'http://opensource.indeedeng.io/imhotep/files/nasa_19950630.22-19950728.12.tsv.gz'
 base_name = os.path.basename(url)
 r = requests.get(url, stream=True)
@@ -10,3 +10,10 @@ with open(base_name, 'wb') as f:
     for chunk in r.raw.stream(1024, decode_content=False):
         if chunk:
             f.write(chunk)
+
+# get filename without extension from the archive file
+file_extns = base_name.split(".")
+new_filename1 = str(file_extns[-5])
+new_filename2 = str(file_extns[-4])
+new_filename3 = str(file_extns[-3])
+new_filename = (".".join([new_filename1,new_filename2,new_filename3]))
